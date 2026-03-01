f162a9f6927d62883160027ae2fbc319ff4eb457
Jose Ivan <jigonzalez930209@gmail.com>
2026-02-28 21:46:04 -0300

    chore: update build and styling configuration

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./docs/.vitepress/theme/**/*.{js,jsx,ts,tsx,vue}",
+    "./docs/**/*.md"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
